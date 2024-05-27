ca2712d906eed19cde7f3f91521c3530e55b4d29
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-05-27 00:11:46 -0300

    configure jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import { pathsToModuleNameMapper } from "ts-jest";
+
+export default {
+  moduleFileExtensions: ["js", "json", "ts"],
+  testRegex: ".*\\..*spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+};
