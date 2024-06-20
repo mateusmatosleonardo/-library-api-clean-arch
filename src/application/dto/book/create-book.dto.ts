6164403dfc1a49e3b7d50db29e784be31ba6326c
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-06-20 16:14:55 -0300

    feat: create book entity

--- src/application/dto/book/create-book.dto.ts
+++ src/application/dto/book/create-book.dto.ts
@@ -1,7 +1,23 @@
-export interface CreateBookDTO {
+export interface IBook {
   name: string;
   quantity: number;
   author: string;
   gender: string;
   isbn: string;
+}
+
+export class Book implements IBook {
+  name: string;
+  quantity: number;
+  author: string;
+  gender: string;
+  isbn: string;
+
+  constructor({ name, quantity, author, gender, isbn }: IBook) {
+    this.name = name;
+    this.quantity = quantity;
+    this.author = author;
+    this.gender = gender;
+    this.isbn = isbn;
+  }
 }
