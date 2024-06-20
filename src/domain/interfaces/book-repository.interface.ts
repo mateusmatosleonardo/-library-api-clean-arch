f922b5f09c0f4be8aac6e684ec205873882165bb
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-06-20 16:27:07 -0300

    refactor: update prisma book adapter

--- src/domain/interfaces/book-repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import { CreateBookDTO } from "application/dto/book/create-book.dto";
-
-export interface BookRepository {
-  create(input: CreateBookDTO): Promise<{ message: string }>;
-}
--- src/infra/db/repositories/prisma-book-repository.ts
+++ src/infra/db/repositories/prisma-book-repository.ts
@@ -1,9 +1,9 @@
-import { CreateBookDTO } from "application/dto/book/create-book.dto";
+import { Book } from "application/dto/book/create-book.dto";
 import { prisma } from "infra/db/prisma";
-import { BookRepository } from "domain/interfaces/book-repository.interface";
+import { BookRepository } from "interfaces/book-repository.interface";
 
 export class PrismaBookRepository implements BookRepository {
-  async create(input: CreateBookDTO): Promise<any> {
+  async create(input: Book): Promise<any> {
     return prisma.book.create({
       data: input,
     });
