001fdc0071f61a34d31fe59eeed90c380d4a8b39
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-06-20 16:15:22 -0300

    refactor: update create book use case

--- src/application/use-cases/book/create-book.use-case.ts
+++ src/application/use-cases/book/create-book.use-case.ts
@@ -1,10 +1,10 @@
-import { BookRepository } from "domain/interfaces/book-repository.interface";
-import { CreateBookDTO } from "application/dto/book/create-book.dto";
+import { BookRepository } from "interfaces/book-repository.interface";
+import { Book } from "application/dto/book/create-book.dto";
 
 export class CreateBookUseCase {
   constructor(private readonly bookRepository: BookRepository) {}
 
-  async execute(input: CreateBookDTO) {
+  async execute(input: Book) {
     return this.bookRepository.create(input);
   }
 }
