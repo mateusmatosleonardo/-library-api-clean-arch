7bab19d6b3f536dfe3a898638e9d278d7097d737
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-06-21 23:18:03 -0300

    refactor: rename files and directories

---
+++ src/application/dto/book/book.dto.ts
@@ -0,0 +1,7 @@
+export interface BookDTO {
+  name: string;
+  quantity: number;
+  author: string;
+  gender: string;
+  isbn: string;
+}
--- src/interfaces/book-repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Book } from "application/dto/book/create-book.dto";
-
-export interface BookRepository {
-  create(input: Book): Promise<{ message: string }>;
-  findByIsbn(isbn: string): Promise<Book | null>;
-  findByNameOrIsbn(name?: string, isbn?: string): Promise<Book | null>;
-}
