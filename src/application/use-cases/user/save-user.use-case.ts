b3ff3e54081aff5a04efa97d86cc6d8cb2e9254a
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-06-25 00:10:13 -0300

    feat: create save user use case

---
+++ src/application/use-cases/user/save-user.use-case.ts
@@ -0,0 +1,20 @@
+import { UserRepository } from "@/application/interfaces/user/user-repository.interface";
+
+type Input = {
+  name: string;
+  email: string;
+  cpf: string;
+  phone: string;
+};
+
+type Output = {
+  message: string;
+};
+
+export class SaveUserUseCase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute(input: Input): Promise<Output> {
+    return this.userRepository.save(input);
+  }
+}
