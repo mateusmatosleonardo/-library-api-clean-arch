4cb841ada3d13565ad329283715763fafa6e770b
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-06-25 01:26:47 -0300

    feat: create find by email and find by cpf in user repository

--- src/application/interfaces/user/user-repository.interface.ts
+++ src/application/interfaces/user/user-repository.interface.ts
@@ -2,4 +2,6 @@ import { User } from "@/application/dto/user/user.dto";
 
 export interface UserRepository {
   save(user: User): Promise<{ message: string }>;
+  findByEmail(email: string): Promise<User | null>;
+  findByCpf(cpf: string): Promise<User | null>;
 }
