f31fd269baa2d3469e85ddfef152e87d3aa59cc1
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-06-25 00:06:45 -0300

    feat: add methods getters and setters for user entity

--- src/domain/entitites/user.entity.ts
+++ src/domain/entitites/user.entity.ts
@@ -27,4 +27,49 @@ export class UserEntity extends Entity {
   getPhone() {
     return this.props.phone;
   }
+
+  private validateEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  private validateCPF(cpf: string): boolean {
+    return cpf.length === 11;
+  }
+
+  private validatePhone(phone: string): boolean {
+    return phone.length === 11;
+  }
+
+  setName(name: string) {
+    this.props.name = name;
+  }
+
+  setEmail(email: string) {
+    if (!this.validateEmail(email)) {
+      throw Error("Invalid email format");
+    }
+    this.props.email = email;
+  }
+
+  setCfp(cpf: string) {
+    if (!this.validateCPF(cpf)) {
+      throw Error("Invalid CPF format");
+    }
+    this.props.cpf = cpf;
+  }
+
+  setPhone(phone: string) {
+    if (!this.validatePhone(phone)) {
+      throw new Error("Invalid phone format");
+    }
+    this.props.phone = phone;
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      ...this.props,
+    };
+  }
 }
