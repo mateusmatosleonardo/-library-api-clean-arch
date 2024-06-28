373144ae79e21e99778282bf71c22ec3f315e009
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-06-27 22:52:08 -0300

    feat: create save loan use case

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,18 +1,19 @@
 import fastify from "fastify";
-import { createUserController } from "presentation/controllers/user/create-user.controller";
-import { findAllUsersController } from "presentation/controllers/user/find-all-users.controller";
-import { FindUserByEmailController } from "presentation/controllers/user/find-user-by-email.controller";
-import { createBookController } from "presentation/controllers/book/create-book.controller";
-import { findBookController } from "presentation/controllers/book/find-book.controller";
+import { saveBookController } from "@/presentation/controllers/book/save-book.controller";
+import { findBookController } from "@/presentation/controllers/book/find-book.controller";
+import { saveUserController } from "@/presentation/controllers/user/save-user.controller";
+import { findAllUsersController } from "@/presentation/controllers/user/find-all-users.controller";
+import { saveLoanController } from "@/presentation/controllers/loan/save-loan.controller";
 
 const app = fastify();
 
-app.register(createUserController);
-app.register(FindUserByEmailController);
+app.register(saveUserController);
 app.register(findAllUsersController);
 
-app.register(createBookController);
+app.register(saveBookController);
 app.register(findBookController);
+
+app.register(saveLoanController);
 
 const PORT = Number(process.env.PORT) || 3000;
 const HOST = String(process.env.HOST) || "0.0.0.0";
