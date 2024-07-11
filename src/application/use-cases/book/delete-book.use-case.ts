f9225ddc443656c9667920d4ca2254715ea96e11
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-07-11 20:22:23 -0300

    feat: create delete book use case

---
+++ src/application/use-cases/book/delete-book.use-case.ts
@@ -0,0 +1,9 @@
+import { BookRepository } from "@/application/interfaces/book/book-repository.interface";
+
+export class DeleteBookUseCase {
+  constructor(private readonly bookRepository: BookRepository) {}
+
+  async execute(id: string) {
+    return this.bookRepository.delete(id);
+  }
+}
