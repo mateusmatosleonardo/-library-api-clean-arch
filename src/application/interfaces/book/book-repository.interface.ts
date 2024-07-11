efda51537dbe88ae1da7a99bf32e6e9c09383c70
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-07-10 21:21:44 -0300

    feat: create delete book use case and delete book controller

--- src/application/interfaces/book/book-repository.interface.ts
+++ src/application/interfaces/book/book-repository.interface.ts
@@ -3,8 +3,9 @@ import { Book } from "@/application/dto/book/book.dto";
 type InputBook = Book & {};
 
 export interface BookRepository {
-  save(input: InputBook): Promise<any>;
+  save(input: InputBook): Promise<Book>;
   findAll(): Promise<Book[] | null>;
   findByIsbn(isbn: string): Promise<Book | null>;
   findByNameOrIsbn(name?: string, isbn?: string): Promise<Book | null>;
+  delete(id: string): Promise<{ message: string }>;
 }
---
+++ src/application/use-cases/book/find-all-books.use-case.ts
@@ -0,0 +1,9 @@
+import { BookRepository } from "@/application/interfaces/book/book-repository.interface";
+
+export class FindAllBooksUseCase {
+  constructor(private readonly bookRepository: BookRepository) {}
+
+  async execute() {
+    return this.bookRepository.findAll();
+  }
+}
