649ffb925cbb51c5729ccfd02917c9752519c9e4
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-08-29 21:39:12 -0300

    update book dto

--- src/application/dto/book/book.dto.ts
+++ src/application/dto/book/book.dto.ts
@@ -1,6 +1,13 @@
+import { AVAILABILITY } from "@/application/enums/availability.enum"
+
 export interface Book {
-  name: string
+  title: string
+  subtitle?: string
   author: string
+  publisher: string
+  publication_date: string
   gender: string
+  edition: string
+  availability: AVAILABILITY
   isbn: string
 }
